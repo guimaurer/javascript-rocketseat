591563d2a63e6fd560b46d349b886801ef768e9a
guimaurer <guilhermemaurer@hotmail.com>
2019-08-27 11:44:47 -0300

    feature app users, modulo de adicionar usuarios apartir da api do gitub

---
+++ app-users/app.js
@@ -0,0 +1,78 @@
+var cardList = document.querySelector("body #renderlist");
+var cardImput = document.querySelector("body #card-input");
+var cardButton = document.querySelector("body #card-button");
+
+var cards = JSON.parse(localStorage.getItem('list_todos')) || [];
+
+function renderCards() {
+  cardList.innerHTML = "";
+  for (card of cards) {
+    var cardDev = document.createElement("div");
+    cardDev.setAttribute("class", "card-itens");
+    var pos = cards.indexOf(card);
+
+    var avatar = document.createElement("img");
+    avatar.setAttribute("src", card.avatar_url);
+    avatar.setAttribute("class", "card-img-top");
+    avatar.setAttribute("alt", "imagem de perfil do desenvolvedor ");
+    cardDev.appendChild(avatar);
+
+    var infoDev = document.createElement("div");
+    infoDev.setAttribute("class", "info-dev");
+
+    var nameDev = document.createElement("h5");
+    var textNamedev = document.createTextNode(card.name);
+    nameDev.appendChild(textNamedev);
+    infoDev.appendChild(nameDev);
+
+    var bioDev = document.createElement("p");
+    bioDev.setAttribute("class", "bio-dev");
+    var textBiodev = document.createTextNode((card.bio==null)? "Usuário sem Bio": card.bio);
+    bioDev.appendChild(textBiodev);
+    infoDev.appendChild(bioDev);
+
+    var link = document.createElement("a");
+    link.setAttribute("href", "#");
+    link.setAttribute("onclick", "excluirTodo(" + pos + ")");
+    var nameLink = document.createTextNode("Excluir");
+    link.appendChild(nameLink);
+    infoDev.appendChild(link);
+
+    cardDev.appendChild(infoDev);
+    cardList.appendChild(cardDev);
+  }
+}
+
+function excluirTodo(pos) {
+  cards.splice(pos, 1);
+  renderCards();
+  saveToStorage() 
+}
+
+function addCard() {
+  var textcard = cardImput.value;
+  if (textcard != "") {
+    
+    axios
+      .get("https://api.github.com/users/" + textcard, "data")
+      .then(res => {
+        cards.push(res.data);
+        renderCards();
+        saveToStorage() 
+      })
+      .catch(err => {
+        console.error(alert("Usuário não indentificado"));
+      });
+
+    
+  } else alert("Conteúdo Vazio");
+
+    cardImput.value = "";
+} 
+cardButton.onclick = addCard;
+
+function saveToStorage() {
+  localStorage.setItem("list_todos", JSON.stringify(cards));
+}
+
+renderCards();
